e8388a28c84b400e911c44d3fad796d758e9d366
Lawrie <dev@lawrieheyfron.co.uk>
2021-10-14 13:48:33 +0100

    Initial Commit

---
+++ Clarity/Clarity.h
@@ -0,0 +1,18 @@
+//
+//  Clarity.h
+//  Clarity
+//
+//  Created by Lawrie Development on 14/10/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Clarity.
+FOUNDATION_EXPORT double ClarityVersionNumber;
+
+//! Project version string for Clarity.
+FOUNDATION_EXPORT const unsigned char ClarityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Clarity/PublicHeader.h>
+
+
